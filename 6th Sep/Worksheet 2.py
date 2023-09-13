6acc0a2aba13ed7d3964b743211133823aa348a9
Lucideds-school <133981612+Lucideds-school@users.noreply.github.com>
2023-09-13 13:41:13 +0000

    edit

--- 6th Sep/Worksheet 2.py
+++ 6th Sep/Worksheet 2.py
@@ -1,11 +1,12 @@
-from time import sleep
+import time
 def twinkle():
     print("Twinkle, twinkle little star")
-    sleep(3)
+    time.sleep(3)
     print("How I wonder what you are")
-    sleep(3)
+    time.sleep(3)
 def upabove():
     print("Up above the world so high")
+    time.sleep(3)
     print("Like a diamond in the sky")
 twinkle()
 upabove()
