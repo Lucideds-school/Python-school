dce67f5f3fb769dc55066a7b91d9bcfdfd6c6892
Lucideds-school <133981612+Lucideds-school@users.noreply.github.com>
2023-09-26 13:05:36 +0000

    lesson

---
+++ 26th sep/Worksheet 1 (with def).py
@@ -0,0 +1,15 @@
+import time
+print("Welcome to the 'JOKE MACHINE'")
+count = 0
+def joke(que, ans, ans2):
+    print("Guess the punchline...")
+    time.sleep(0.8)
+    x = input(que).lower()
+    if x == ans.lower() or x == ans2.lower():
+        print("Correct!")
+        count = count =+ 1
+    else:
+        print("Wrong. It is","'"+ans+"'")
+
+joke("What is pink and fluffy? ", "Pink fluff", "Pink fluff")
+
