425f55a77f4e16c7669e4f282cdb56ad92f85115
Lucideds-school <133981612+Lucideds-school@users.noreply.github.com>
2023-09-26 13:08:58 +0000

    edit

--- 26th sep/Worksheet 1 (with def).py
+++ 26th sep/Worksheet 1 (with def).py
@@ -12,4 +12,8 @@ def joke(que, ans, ans2):
         print("Wrong. It is","'"+ans+"'")
 
 joke("What is pink and fluffy? ", "Pink fluff", "Pink fluff")
+joke("What is brown and sticky? ", "A brown stick", "brown stick")
+joke("What is black and white and red all over? ", "A newspaper", "newspaper")
+
+
 
